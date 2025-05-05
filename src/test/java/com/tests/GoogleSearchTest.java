b615936082223752869113e1687e944943c052e0
sathiyam <sathiyam@cisco.com>
2025-05-05 19:35:51 +0530

    updated

--- src/test/java/com/tests/GoogleSearchTest.java
+++ src/test/java/com/tests/GoogleSearchTest.java
@@ -1,13 +1,10 @@
 package com.tests;
 
-import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
