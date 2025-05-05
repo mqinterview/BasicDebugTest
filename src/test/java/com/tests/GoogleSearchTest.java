4fad118d67bc09ba8cccdc499fdc08df1c8bd6d2
sathiyam <sathiyam@cisco.com>
2025-05-05 19:19:46 +0530

    updated

---
+++ src/test/java/com/tests/GoogleSearchTest.java
@@ -0,0 +1,47 @@
+package com.tests;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+	WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testGoogleSearch() {
+    	driver.get("https://duckduckgo.com");
+
+        WebElement searchBox = driver.findElement(By.id("q"));
+        searchBox.sendKeys("web.webex.com");
+        searchBox.submit();
+
+        System.out.println("Title : " + driver.getTitle().toLowerCase());
+        Assert.assertTrue(driver.getTitle().toLowerCase().contains("webex"),
+            "Title does not contain 'webex'");
+        
+        
+        Assert.assertTrue(driver.getTitle().contains("webex"), "Title does not contain 'webex'");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null)
+            driver.quit();
+    }
+
+}
